b80b0b07433b20f77e15d6c33c2117f8968edc6e
panospd <anastapd@gmail.com>
2020-10-02 20:44:03 +0100

    Fixed logo bug

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import Screen from "../app/components/Screen";
 
 export default function WelcomeScreen({ createNewGame, responses }) {
   return (
     <Screen>
-      <Image style={styles.image} source={require("./assets/logo.png")} />
+      <Image style={styles.image} source={require("../assets/logo.png")} />
       {responses.length !== 0 && <ScoreContainer responses={responses} />}
       <Button title="New Game" onPress={createNewGame} />
     </Screen>
