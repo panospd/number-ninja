7dea889be56e034483bace3bb9842b25751d33b3
panospd <anastapd@gmail.com>
2020-10-04 20:59:27 +0100

    Reverted background color

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -8,7 +8,7 @@ export default function Screen(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ccc",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
